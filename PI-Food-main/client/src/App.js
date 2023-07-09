d47545ce445a593a7a9a1af50ffa59b61ae0e67e
LA ROSA DANELA <danielaalarosa@gmail.com>
2023-07-09 14:02:24 -0300

    Arreglos en rutas

--- PI-Food-main/client/src/App.js
+++ PI-Food-main/client/src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import Landing from "./pages/landing";
+import Home from "./pages/home";
+import Detail from "./pages/detail";
+import Create from "./pages/create";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          {/* <h1>Henry Food</h1> */}
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/recipes/:id" component={Detail} />
+          <Route exact path="/create" component={Create} />
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
