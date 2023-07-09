cbe13ae83ef575d8da60bd1b64249aac3406a2dc
LA ROSA DANELA <danielaalarosa@gmail.com>
2023-07-09 13:59:32 -0300

    Creacion de comp paginacion

---
+++ PI-Food-main/client/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import styles from "./pagination.module.css";
+
+export default function Pagination({ page, onClickNextPage, onClickPrevPage }) {
+  return (
+    <div className="pagination">
+      {page > 1 && (
+        <IoIosArrowBack className={styles.prev} onClick={onClickPrevPage} />
+      )}
+      <span>{page}</span>
+      <IoIosArrowForward className={styles.next} onClick={onClickNextPage} />
+    </div>
+  );
+}
