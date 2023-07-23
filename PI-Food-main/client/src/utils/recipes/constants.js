ad0523ee92456b484443cd221f5a22fe314d972a
LA ROSA DANELA <danielaalarosa@gmail.com>
2023-07-23 20:44:23 -0300

    Creacion de constantes de filtro origin

--- PI-Food-main/client/src/utils/recipes/constants.js
+++ PI-Food-main/client/src/utils/recipes/constants.js
@@ -2,3 +2,5 @@ export const ASCENDENT = "asc";
 export const DESCENDENT = "desc";
 export const HS_ASCENDENT = "hsAsc";
 export const HS_DESCENDENT = "hsDesc";
+export const ORIGIN_DB = "DB";
+export const ORIGIN_API = "API";
