c76e621af5fed7c0ca829a84f8f20b487b9ea78a
LA ROSA DANELA <danielaalarosa@gmail.com>
2023-07-23 20:52:39 -0300

    Mensaje de not found recipe

--- PI-Food-main/client/src/components/notFound/NotFound.jsx
+++ PI-Food-main/client/src/components/notFound/NotFound.jsx
@@ -4,9 +4,7 @@ import style from "./notFound.module.css";
 function NotFound() {
   return (
     <div className={style.conteiner}>
-      <h2 className={style.p}>
-        No recipe found, <br /> re-enter a name.
-      </h2>
+      <h2 className={style.p}>No recipe found.</h2>
     </div>
   );
 }
