f80d8d417a19f2b76819915b05e03787846955a8
LA ROSA DANELA <danielaalarosa@gmail.com>
2023-07-24 14:12:59 -0300

    Correccion de nombre de constante

--- PI-Food-main/api/src/controllers/diet.js
+++ PI-Food-main/api/src/controllers/diet.js
@@ -6,10 +6,10 @@ const { getRecipesApi } = require("./recipes.js");
 // Obtener todos los tipos de dieta posibles
 const getDietsBD = async (req, res) => {
   try {
-    const prueba = await Diet.findAll();
-    res.status(200).json(prueba);
+    const dietsTypesAll = await Diet.findAll();
+    res.status(200).json(dietsTypesAll);
   } catch (error) {
-    res.status(404).send({ msg: "No buco dietas" });
+    res.status(404).send({ msg: "No busco dietas" });
   }
 };
 // En una primera instancia, cuando no exista ninguno, deberán precargar la base de datos con los tipos de datos indicados por spoonacular acá
@@ -30,7 +30,7 @@ const populateDBWithDiet = async () => {
       .split(",");
     let b = new Set(a);
     let c = [...b];
-    let dietsTypes = c.filter((e) => e !== "").slice(1);
+    let dietsTypes = c.filter((e) => e !== "");
 
     dietsTypes.map((diet) => Diet.findOrCreate({ where: { name: diet } }));
     console.log("populateDBWithDiet");
